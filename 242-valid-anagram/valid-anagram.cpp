3fe45cf02599d6b5790ae006ebc75217e5a4906a
Shanti Prakash Singh <ssurya32746@gmail.com>
2026-07-16 16:51:17 +0530

    Time: 3 ms (45.94%) | Memory: 9.7 MB (50.01%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
-{
-    return true;
-}        
-return false;
-        
+        if(s.size()!=t.size()){
+            return false;
+        }
+        int arr[26]={0};
+        for(int i=0;s.size()>i;i++){
+            arr[s[i]-97]++;
+            arr[t[i]-97]--;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0)
+            return false;
+        }
+        return true;
 
         
     }
