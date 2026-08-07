dc197b9e0157a8162d587130eb2d7db42027a198
Shanti Prakash Singh <ssurya32746@gmail.com>
2026-08-08 00:17:30 +0530

    Time: 168 ms (5.04%) | Memory: 23.7 MB (99.00%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>v;
-        vector<int>p;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                v.push_back(nums[i]);
-            }
-            else{
-                p.push_back(nums[i]);
-            }
-        }
-        for(int i=0;i<p.size();i++){
-            v.push_back(p[i]);
+    int j=0;
+    for(int i=0;i<nums.size();i++){
+    if(nums[i]!=0){
+        swap(nums[i],nums[j]);
+        j++;
+    }
+    }
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
 
-        }
-        nums=v;
-        for(int i=0;i<v.size();i++){
-            cout<<v[i]<<"  ";
-        }
     }
 };
